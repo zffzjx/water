b53af8f20f155a47643ef1276b48786367600a6a
hncg <574878649@qq.com>
2016-05-01 22:33:42 +0800

    测试数据库

--- water/handler/__init__.py
+++ water/handler/__init__.py
@@ -2,5 +2,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql://cg:123456@localhost:3306/water?charset=utf8&unix_socket=/var/mysql/data/mysql.sock', echo=False) # noqa
+engine = create_engine('mysql://cg:123456@localhost:3306/water_test?charset=utf8&unix_socket=/var/mysql/5.7.10/data/mysql.sock', echo=False) # noqa
 DBSession = sessionmaker(bind=engine)
